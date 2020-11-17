cd11102a38ad9dbb4a65ef93d5a71b26b70f530a
Radwan Suufi <rd.90@hotmail.com>
2020-11-17 23:56:23 +0000

    Configure application for seeding Database.

--- Startup.cs
+++ Startup.cs
@@ -1,29 +1,53 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using WebAppExample.Models;
 
 namespace WebAppExample
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        private readonly IConfiguration Configuration;
+        private IWebHostEnvironment Environment;
+        public Startup(IConfiguration config, IWebHostEnvironment env)
+        {
+            Configuration = config;
+            Environment = env;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionStrings:ProductConnection"]);
+
+                if (Environment.IsDevelopment())
+                {
+                    options.EnableSensitiveDataLogging(true);
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, DataContext context,
+            IHostApplicationLifetime lifetime)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                bool runSeedDataBase;
+                if (bool.TryParse(Configuration["INITDB"], out runSeedDataBase) &&
+                     runSeedDataBase)
+                {
+                    SeedData.SeedDataBase(context);
+                    lifetime.StopApplication();
+                }
             }
 
             app.UseRouting();
@@ -32,9 +56,12 @@ namespace WebAppExample
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World!\n");
+                    await context.Response.WriteAsync(Environment.IsDevelopment().ToString());
                 });
             });
+
+
         }
     }
 }
