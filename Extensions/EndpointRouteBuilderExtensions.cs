cd93ac2ec28c7a8ecd5ce56ffdd70df0bf1c31fe
Radwan Suufi <rd.90@hotmail.com>
2020-11-18 00:28:59 +0000

    Create Extension method.
    This allows middleware components to be used as endpoints.

---
+++ Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Builder;
+
+namespace WebAppExample.Extensions
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        public static IEndpointConventionBuilder UseMiddleWareAsEndpoint<T>(this IEndpointRouteBuilder endpoints, string path)
+        {
+
+            var app = endpoints.CreateApplicationBuilder();
+
+            app.UseMiddleware<T>();
+
+            var requestDelegate = app.Build();
+
+            return endpoints.MapGet(path, requestDelegate);
+        }
+        
+    }
+}
